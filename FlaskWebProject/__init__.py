a923a316a42ec9c17705c2561a57cdd8bc1eb664
vaishnavi694 <mvaishnavi0609@gmail.com>
2026-03-15 21:51:40 +0530

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -2,7 +2,6 @@
 The flask application package.
 """
 import logging
-from logging import StreamHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -11,27 +10,10 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
-# Logging configuration
-handler = StreamHandler()
-handler.setLevel(logging.INFO)
-
-app.logger.addHandler(handler)
-app.logger.setLevel(logging.INFO)
-
+# TODO: Add any logging levels and handlers with app.logger
 Session(app)
-
 db = SQLAlchemy(app)
-
 login = LoginManager(app)
 login.login_view = 'login'
-
-
-# IMPORTANT: Flask-Login user loader
-@login.user_loader
-def load_user(id):
-    from FlaskWebProject.models import User
-    return User.query.get(int(id))
-
 
 import FlaskWebProject.views
